b3e38191d309bba7aa0ea2318a3b2e43527d2fa1
AilinCarribero <42686226+AilinCarribero@users.noreply.github.com>
2021-11-24 10:42:00 -0300

    Se arreglo: -Guardado de egresos e ingresos

--- src/controllers/egresosController.js
+++ src/controllers/egresosController.js
@@ -3,39 +3,50 @@ const sql = require('../sql/egresosQuery');
 
 //Agregar egreso
 exports.insertEgreso = async (req, res) => {
-    const datos = req.body;
+    const datos = !req.body.length ? [req.body] : req.body;;
 
     try {
         //Inserta el nuevo egreso
         datos.forEach(dato => {
+            console.log('hola 1')
             if (!dato.fecha_diferido_pago) {
-                dato.fecha_diferido_pago = NULL;
-            };
+                dato.fecha_diferido_pago = '0-0-0';
+            }
+            console.log('hola 1.1')
             if (!dato.cuota) {
                 dato.cuota = 0;
             }
+            console.log('hola 1.2')
             if (!dato.cuotaNumero) {
                 dato.cuotaNumero = 0;
             }
+            console.log('hola 1.3')
             if (!dato.observaciones) {
                 dato.observaciones = '';
             }
+            console.log('hola 1.4')
             if (!dato.id_detalle_ac) {
                 dato.id_detalle_ac = 0;
             }
-
+            console.log('hola 1.5')
             bd.query(sql.insertEgreso(dato), async (err, response) => {
+                console.log('hola query')
+                console.log(datos)
                 if (err) {
+                    err.statusText = "Error";
+                    err.status = 400;
+
                     res.json(err);
+                    console.log(err)
                 }
                 if (response) {
                     response.statusText = "Ok";
                     response.status = 200;
+
+                    res.json(response);
                 }
             })
         });
-        res.json(response);
-        res.end();
     } catch (error) {
         return res.json(error);
     }
--- src/controllers/ingresosController.js
+++ src/controllers/ingresosController.js
@@ -3,14 +3,14 @@ const sql = require('../sql/ingresosQuery');
 
 //Agregar ingreso
 exports.insertIngreso = async (req, res) => {
-    const datos = req.body;
+    const datos = !req.body.length ? [req.body] : req.body;
 
     try {
         //Inserta el nuevo ingreso
-        datos.forEach(dato => {
+        datos.forEach(async dato => {
             if (!dato.fecha_diferido_cobro) {
-                dato.fecha_diferido_cobro = NULL;
-            };
+                dato.fecha_diferido_cobro = '0-0-0';
+            }
             if (!dato.cuota) {
                 dato.cuota = 0;
             }
@@ -20,19 +20,21 @@ exports.insertIngreso = async (req, res) => {
             if (!dato.observaciones) {
                 dato.observaciones = '';
             }
-
+            
             bd.query(sql.insertIngreso(dato), async (err, response) => {
                 if (err) {
-                    res.json(err);
-                }
-                if (response) {
+                    err.statusText = "Error";
+                    err.status = 400;
+
+                    res.json(err)
+                } else if (response) {
                     response.statusText = "Ok";
                     response.status = 200;
+
+                    res.json(response);
                 }
             })
-        });
-        res.json(response);
-        res.end();
+        })
     } catch (error) {
         return res.json(error);
     }
