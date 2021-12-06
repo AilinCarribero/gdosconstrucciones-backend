5f91813eb1758e64cc54bd5cb7e0bca9f5cefa2c
AilinCarribero <42686226+AilinCarribero@users.noreply.github.com>
2021-12-06 20:37:14 -0300

    Parte del commit anterior

--- src/sql/db_tables.sql
+++ src/sql/db_tables.sql
@@ -41,6 +41,7 @@ create table centro_costo (id_centro_costo int auto_increment primary key, tipo_
 /* Insert new value so we can test the methods */
 insert into centro_costo (tipo_centro_costo,siglas_cc) value ("Centro de Costo Común", "CCC"); /*id = 1*/
 insert into centro_costo (tipo_centro_costo,siglas_cc) value ("Centro de Costo de Proyecto", "CCP"); /*id = 2*/
+insert into centro_costo (tipo_centro_costo,siglas_cc) value ("Centro de Costo de Empresa", "CCE"); /*id = 3*/
 
 /*-------------------------------------------------------------------------------------------------------------------------*/
 /* Create new table */
